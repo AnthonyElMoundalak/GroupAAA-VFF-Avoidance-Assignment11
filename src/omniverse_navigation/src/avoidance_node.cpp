7e38d09fc43a8275686c81972a83bf1491e66d4f
Ahmad Chaachouh <ahmad.chaachouh.cad@gmail.com>
2024-08-13 01:03:57 +0300

    Add visualizations via Markers

--- src/omniverse_navigation/src/avoidance_node.cpp
+++ src/omniverse_navigation/src/avoidance_node.cpp
@@ -3,9 +3,12 @@
 #include <cmath>
 
 AvoidanceNode::AvoidanceNode() : Node("avoidance_node") {
+    rclcpp::QoS qos_profile(rclcpp::KeepLast(10));
+    qos_profile.durability(RMW_QOS_POLICY_DURABILITY_VOLATILE);
     subscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
         "/scan", 10, std::bind(&AvoidanceNode::laser_callback, this, std::placeholders::_1));
     publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
+    rviz_pub_ = this->create_publisher<visualization_msgs::msg::MarkerArray>("debugging", qos_profile);
 
     repulsive_strength_ = 0.05;
     repulsive_distance_ = 0.55;
@@ -38,6 +41,7 @@ void AvoidanceNode::timer_callback(){
     }
 
     publisher_->publish(cmd_vel_msg_);
+    publish_markers();
 
 }
 
@@ -46,6 +50,12 @@ void AvoidanceNode::calculate_forces(){
 
     float repulsive_force_x = 0.0;
     float repulsive_force_y = 0.0;
+
+    repulsive_force_.resize(2, 0.0);
+    attractive_force_.resize(2, 0.0);
+    resultant_force_.resize(2, 0.0);
+
+    
 
     double closest_obstacle_distance = *std::min_element(laser_scan_->ranges.begin(), laser_scan_->ranges.end());
     auto min_element_iter = std::min_element(laser_scan_->ranges.begin(), laser_scan_->ranges.end());
@@ -57,13 +67,24 @@ void AvoidanceNode::calculate_forces(){
         if (force > 0.6) force = 0.6;
         repulsive_force_x += -force * std::cos(angle);
         repulsive_force_y += -force * std::sin(angle);
+        repulsive_force_.clear();
+        repulsive_force_.push_back(repulsive_force_x);
+        repulsive_force_.push_back(repulsive_force_y);
+
     }
 
     float attractive_force_x = 0.5;
     float attractive_force_y = 0.0;
+    attractive_force_.clear();
+    attractive_force_.push_back(attractive_force_x);
+    attractive_force_.push_back(attractive_force_y);
 
     float resultant_force_x = attractive_force_x + repulsive_force_x;
     float resultant_force_y = attractive_force_y + repulsive_force_y;
+    
+    resultant_force_.clear();
+    resultant_force_.push_back(resultant_force_x);
+    resultant_force_.push_back(resultant_force_y);
 
     cmd_vel_msg_.linear.x = resultant_force_x;
     cmd_vel_msg_.angular.z = std::atan2(resultant_force_y, resultant_force_x);
@@ -71,39 +92,140 @@ void AvoidanceNode::calculate_forces(){
 }
 
 
+void AvoidanceNode::publish_markers(){
 
-// void AvoidanceNode::laser_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
-//     float repulsive_force_x = 0.0;
-//     float repulsive_force_y = 0.0;
+     // Delete old markers
+    for (auto& marker : rviz_msg_.markers) {
+        marker.action = visualization_msgs::msg::Marker::DELETE;
+    }
+    rviz_pub_->publish(rviz_msg_);
 
-//     for (size_t i = 0; i < msg->ranges.size(); ++i) {
-//         float range = msg->ranges[i];
-//         if (range < msg->range_max && range > msg->range_min) {
-//             float angle = msg->angle_min + i * msg->angle_increment;
-//             float force = 0.1 / range; // simple inverse distance weighting
-//             repulsive_force_x += force * std::cos(angle);
-//             repulsive_force_y += force * std::sin(angle);
-//         }
-//     }
+    // Clear the marker array for new markers
+    rviz_msg_.markers.clear();
+    int marker_id = 0;
 
-//     // Normalize the repulsive forces
-//     float magnitude = std::sqrt(repulsive_force_x * repulsive_force_x + repulsive_force_y * repulsive_force_y);
-//     if (magnitude > 0.0) {
-//         repulsive_force_x /= magnitude;
-//         repulsive_force_y /= magnitude;
-//     }
+    visualization_msgs::msg::Marker repulsive_marker;
+    repulsive_marker.header.frame_id = "base_link";
+    repulsive_marker.header.stamp = this->now();
+    repulsive_marker.ns = "repulsive_force";
+    repulsive_marker.id = marker_id++;
+    repulsive_marker.type = visualization_msgs::msg::Marker::ARROW;
+    repulsive_marker.action = visualization_msgs::msg::Marker::ADD;
+    repulsive_marker.pose.orientation.w = 1.0;
+    repulsive_marker.scale.x = 0.02;
+    repulsive_marker.scale.y = 0.05;
+    repulsive_marker.scale.z = 0.05;
+    repulsive_marker.color.a = 1.0; 
+    repulsive_marker.color.r = 1.0;
+    repulsive_marker.color.g = 0.0;
+    repulsive_marker.color.b = 0.0;
 
-//     // Compute the desired velocity
-//     float linear_x = 0.2 - repulsive_force_x;
-//     float angular_z = -repulsive_force_y;
+    geometry_msgs::msg::Point start;
+    start.x = 0;
+    start.y = 0;
+    start.z = 0;
+    
 
-//     auto twist_msg = geometry_msgs::msg::Twist();
-//     twist_msg.linear.x = linear_x;
-//     twist_msg.angular.z = angular_z;
+    geometry_msgs::msg::Point end;
+   if (repulsive_force_.size() >= 2) {
+    end.x = repulsive_force_[0];
+    end.y = repulsive_force_[1];
+    end.z = 0;
+} else {
+    // Handle the error or initialize with default values
+    end.x = 0.0;
+    end.y = 0.0;
+    end.z = 0.0;
+}
+    
 
-//     publisher_->publish(twist_msg);
-// }
+    repulsive_marker.points.push_back(start);
+    repulsive_marker.points.push_back(end);
 
+    rviz_msg_.markers.push_back(repulsive_marker);
+
+    // Add attractive force to marker array
+    visualization_msgs::msg::Marker attractive_marker;
+    attractive_marker.header.frame_id = "base_link";
+    attractive_marker.header.stamp = this->now();
+    attractive_marker.ns = "attractive_force";
+    attractive_marker.id = marker_id++;
+    attractive_marker.type = visualization_msgs::msg::Marker::ARROW;
+    attractive_marker.action = visualization_msgs::msg::Marker::ADD;
+    attractive_marker.pose.orientation.w = 1.0;
+    attractive_marker.scale.x = 0.02;
+    attractive_marker.scale.y = 0.05;
+    attractive_marker.scale.z = 0.05;
+    attractive_marker.color.a = 1.0;
+    attractive_marker.color.r = 0.0;
+    attractive_marker.color.g = 0.0;
+    attractive_marker.color.b = 1.0;
+
+    geometry_msgs::msg::Point attractive_start;
+    attractive_start.x = 0;
+    attractive_start.y = 0;
+    attractive_start.z = 0;
+   
+
+    geometry_msgs::msg::Point attractive_end;
+   if (attractive_force_.size() >= 2) {
+    attractive_end.x = attractive_force_[0];
+    attractive_end.y = attractive_force_[1];
+    attractive_end.z = 0.0;
+} else {
+    // Handle the error or initialize with default values
+    attractive_end.x = 0.0;
+    attractive_end.y = 0.0;
+    attractive_end.z = 0.0;
+}
+    
+
+    attractive_marker.points.push_back(attractive_start);
+    attractive_marker.points.push_back(attractive_end);
+
+    rviz_msg_.markers.push_back(attractive_marker);
+
+    // Add resultant force to marker array
+    visualization_msgs::msg::Marker resultant_marker;
+    resultant_marker.header.frame_id = "base_link";
+    resultant_marker.header.stamp = this->now();
+    resultant_marker.ns = "resultant_force";
+    resultant_marker.id = marker_id++;
+    resultant_marker.type = visualization_msgs::msg::Marker::ARROW;
+    resultant_marker.action = visualization_msgs::msg::Marker::ADD;
+    resultant_marker.pose.orientation.w = 1.0;
+    resultant_marker.scale.x = 0.02;
+    resultant_marker.scale.y = 0.05;
+    resultant_marker.scale.z = 0.05;
+    resultant_marker.color.a = 1.0;
+    resultant_marker.color.r = 0.0;
+    resultant_marker.color.g = 1.0;
+    resultant_marker.color.b = 0.0;
+
+    geometry_msgs::msg::Point resultant_start;
+    resultant_start.x = 0;
+    resultant_start.y = 0;
+    resultant_start.x = 0;
+
+    geometry_msgs::msg::Point resultant_end;
+   if (resultant_force_.size() >= 2) {
+    resultant_end.x = resultant_force_[0];
+    resultant_end.y = resultant_force_[1];
+    resultant_end.z = 0.0;
+} else {
+    // Handle the error or initialize with default values
+    resultant_end.x = 0.0;
+    resultant_end.y = 0.0;
+}
+
+    resultant_marker.points.push_back(resultant_start);
+    resultant_marker.points.push_back(resultant_end);
+
+    rviz_msg_.markers.push_back(resultant_marker);
+
+    rviz_pub_->publish(rviz_msg_);
+
+}
 
 
 int main(int argc, char *argv[]) {
